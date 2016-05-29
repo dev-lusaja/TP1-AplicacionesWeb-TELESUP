e22647a5fc5a44d2ebe6d18bf7ac3e8234bea45c
Oscar Sanchez <oscar_sanchez39@hotmail.com>
2016-05-29 17:22:58 -0500

    Se actualizo backup de la base de datos.

--- DataBase/proyecto2016.sql
+++ DataBase/proyecto2016.sql
@@ -1,20 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 30-05-2016 a las 00:16:45
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.5.35
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `proyecto2016`
@@ -458,7 +445,3 @@ ALTER TABLE `compra_venta`
 --
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Tipo_Usuario_CodTipo`) REFERENCES `tipo_usuario` (`CodTipo`) ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
